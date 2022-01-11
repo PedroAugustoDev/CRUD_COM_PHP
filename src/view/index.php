4a22dd6098ddbd7394c87831287644257593400d
PedroAugustoDev <74946703+PedroAugustoDev@users.noreply.github.com>
2022-01-10 22:45:32 -0300

    up

--- src/view/index.php
+++ src/view/index.php
@@ -98,7 +98,6 @@ $listAll = $dao->findAll();
   }
   ?>
   </table>
-  <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
   <script>
 
     function closeModal(){ $('#exampleModalCenter').modal('hide'); }
